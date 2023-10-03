9ab6e1d499cd2deada5a887d160767f0d62239da
victro-nuzhniy <nuzhniyva@gmail.com>
2023-10-03 19:45:27 +0300

    dev created MainPage.jsx

---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from 'axios';
+
+const MainPage = (props) => {
+    const token = localStorage.getItem('token')
+
+    return (
+        <h1>Hello World!</h1>
+    )
+};
+
+export default MainPage;
