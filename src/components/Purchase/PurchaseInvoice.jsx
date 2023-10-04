3104f42dca6dc227c40e1e440618cd102df20a07
victro-nuzhniy <nuzhniyva@gmail.com>
2023-10-04 21:38:09 +0300

    dev added pagination to PurchaseInvoice

--- src/components/Purchase/PurchaseInvoice.jsx
+++ src/components/Purchase/PurchaseInvoice.jsx
@@ -5,9 +5,13 @@ import HOST from './../../Constants.js';
 
 const PurchaseInvoicePage = () => {
     const [purchaseRegistry, setPurchaseRegistry] = React.useState([])
+    const [pagin, setPagin] = React.useState({
+        offset: 0,
+        limit: 20,
+    })
     const getPurchaseRegistry = async () => {
         const response = await axios.get(
-        `${HOST}/purchase-registry/`,
+        `${HOST}/purchase-registry/?offset=${pagin.offset}&limit=${pagin.limit}`,
         {
             headers: {"Authorization": `Bearer ${localStorage.getItem('token')}`}
         },
@@ -17,12 +21,55 @@ const PurchaseInvoicePage = () => {
             console.log("Something went wrong. May be auth token is invalid.")
         })
     }
+    function handleLeftClick(event) {
+        if (pagin.offset > 0) {
+            setPagin(prevPagin => ({
+                ...prevPagin,
+                offset: prevPagin.offset - prevPagin.limit > 0 ? prevPagin.offset - prevPagin.limit : 0,
+            }))
+        }
+    }
+    function handleRightClick(event){
+        if (purchaseRegistry.length % pagin.limit === 0) {
+            setPagin(prevPagin => ({
+                ...prevPagin,
+                offset: prevPagin.offset + prevPagin.limit
+            }))
+        }
+    }
     React.useEffect(() => {
         getPurchaseRegistry()
-    }, [])
+    }, [pagin])
     return (
         <div>
-            <table class="table">
+            <div>
+                <div className="d-flex flex-row align-items-center">
+                    <div>
+                        <button
+                            onClick={handleLeftClick}
+                            className={pagin.offset > 0 ? "text-dark": "text-info"}
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-left" viewBox="0 0 16 16">
+                              <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+                            </svg>
+                        </button>
+                        <button
+                            onClick={handleRightClick}
+                            className={purchaseRegistry.length % pagin.limit === 0 ? "text-dark" : "text-info"}
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
+                              <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+                            </svg>
+                        </button>
+                    </div>
+                    <h6>Надходження товарів і послуг</h6>
+                </div>
+                <div>
+                    <button>Надходження товарів</button>
+                    <button>Знайти</button>
+                </div>
+            </div>
+            <table className="table">
                 <thead>
                     <tr>
                         <th scope="col">Дата</th>
