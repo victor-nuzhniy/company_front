7dc39b48f4d900bcc4e8f768ffa867f5dfa9f204
victro-nuzhniy <nuzhniyva@gmail.com>
2023-10-06 15:49:42 +0300

    dev refactor routes in App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import MainContent from './components/MainContent';
 import Login from './components/auth/Login';
 import Logout from './components/auth/Logout';
 import PrivateRoute from './components/auth/PrivateRoute';
+import PurchaseInvoice from './components/Purchase/PurchaseInvoice';
+import PurchaseInvoiceRegistry from './components/Purchase/PurchaseInvoiceRegistry';
 
 
 function App() {
@@ -17,7 +19,11 @@ function App() {
   <>
     <Routes>
         <Route path="/" element={<PrivateRoute />}>
-            <Route path="/" element={<MainContent />} />
+            <Route path="/" element={<MainContent />}>
+                <Route index element={<PurchaseInvoiceRegistry />} />
+                <Route path="purchase-invoice-registry" element={<PurchaseInvoiceRegistry />} />
+                <Route path="purchase-invoice" element={<PurchaseInvoice />} />
+            </Route>
         </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<Logout />} />
