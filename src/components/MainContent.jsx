098924dbf5f0e44222083d3a102486a0db5263f1
victro-nuzhniy <nuzhniyva@gmail.com>
2023-10-06 15:50:21 +0300

    dev refactor MainContent - added links

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import axios from 'axios';
 import Login from './auth/Login';
 import MainPage from './MainPage/MainPage';
+import { Link, Outlet } from "react-router-dom";
+import PurchaseInvoiceRegistry from './Purchase/PurchaseInvoiceRegistry';
+import CreatePurchaseInvoice from './Purchase/CreatePurchaseInvoice';
 
 
 const MainContent = (props) => {
+
+    const token = localStorage.getItem('token')
     return(
-        <MainPage
-            url={props.url}
-            setUrl={props.setUrl}
-            host={props.host}
-        />
+        <>
+            <Link to="purchase-invoice-registry">Purchase invoice registry</Link>
+            <Link to="purchase-invoice" state={{ purchaseInvoiceId: ''}}>Purchase invoice</Link>
+            <Outlet />
+        </>
     )
 }
 
-export default MainContent
+export default MainContent;
