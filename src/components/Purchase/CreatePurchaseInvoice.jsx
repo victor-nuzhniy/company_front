fae2ce241fb9d28494a245b0081aa0434e710c08
victro-nuzhniy <nuzhniyva@gmail.com>
2023-10-06 19:45:22 +0300

    dev refactor CreatePurchaseInvoiceModal.jsx

--- src/components/Purchase/CreatePurchaseInvoice.jsx
+++ src/components/Purchase/CreatePurchaseInvoice.jsx
@@ -53,7 +53,7 @@ const CreatePurchaseInvoice = (props) => {
             invoice,
             {headers: {"Authorization": `Bearer ${localStorage.getItem('token')}`}},
         ).then((response) => {
-            props.setNewInvoice(prev => (response.data))
+            props.setPurchaseInvoice(response.data)
         }).catch((error) => {
             console.log("Something went wrong. May be auth token is invalid.")
         })
@@ -68,14 +68,36 @@ const CreatePurchaseInvoice = (props) => {
     function handleCounterpartyChange(event) {
         const {name, value} = event.target
         setCounterpartyId(value)
-    }
+    };
     function handleSubmit(event) {
         event.preventDefault()
         sendPurchaseInvoice()
     };
     return (
         <>
-            <form onSubmit={handleSubmit}>
+            <form
+                onSubmit={handleSubmit}
+                className="modal fade"
+                id="CreatePurchaseInvoiceModal"
+                tabIndex="-1"
+                aria-labelledby="CreatePurchaseInvoiceModalLabel"
+                aria-hidden="true"
+            >
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                      <div className="modal-header">
+                        <h1
+                            className="modal-title fs-5"
+                            id="CreatePurchaseInvoiceModalLabel"
+                        >Modal title</h1>
+                        <button
+                            type="button"
+                            className="btn-close"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"
+                        ></button>
+                      </div>
+                      <div className="modal-body">
                 <input
                     type="text"
                     name="name"
@@ -119,7 +141,20 @@ const CreatePurchaseInvoice = (props) => {
                         )
                     })}
                 </select>
-                <button type="submit">Create purchase invoice</button>
+                        </div>
+                      <div className="modal-footer">
+                        <button
+                            type="button"
+                            className="btn btn-secondary"
+                            data-bs-dismiss="modal"
+                        >Close</button>
+                        <button
+                            type="button"
+                            className="btn btn-primary"
+                        >Save changes</button>
+                      </div>
+                    </div>
+                </div>
             </form>
         </>
     )
