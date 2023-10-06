3c3236254a0812e040b57b83e1fbf45031b8e35e
victro-nuzhniy <nuzhniyva@gmail.com>
2023-10-06 22:53:14 +0300

    dev added modal features to CreatePurchaseInvoiceProduct, refactor: deleted unused states, added props, created logic for adding product

--- src/components/Purchase/CreatePurchaseInvoiceProduct.jsx
+++ src/components/Purchase/CreatePurchaseInvoiceProduct.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import HOST from './../../Constants.js';
 import {pad} from './../common/Func';
 
-const CreatePurchaseInvoiceProduct = () => {
+const CreatePurchaseInvoiceProduct = (props) => {
     const [invoiceProduct, setInvoiceProduct] = React.useState({
         product_id: '',
         quantity: '',
@@ -12,17 +12,20 @@ const CreatePurchaseInvoiceProduct = () => {
         products_left: '',
     });
     const [products, setProducts] = React.useState([])
-    const [invoices, setInvoices] = React.useState([])
-    const [newInvoice, setNewInvoice] = React.useState({
-        name: '', agreement_id: ''
-    })
     const sendPurchaseInvoiceProduct = async () => {
         await axios.post(
             `${HOST}/purchase-invoice-product/`,
             invoiceProduct,
             {headers: {"Authorization": `Bearer ${localStorage.getItem('token')}`}},
         ).then((response) => {
-
+            setInvoiceProduct({
+                product_id: '',
+                quantity: '',
+                price: '',
+                purchase_invoice_id: '',
+                products_left: '',
+            })
+            props.setProductNumber(prev => (prev + 1))
         }).catch((error) => {
             console.log("Something went wrong. May be auth token is invalid.")
         })
@@ -37,84 +40,99 @@ const CreatePurchaseInvoiceProduct = () => {
             console.log("Something went wrong. May be auth token is invalid.")
         })
     }
-    const getInvoices = async () => {
-        await axios.get(
-            `${HOST}/purchase-invoice/`,
-            {headers: {"Authorization": `Bearer ${localStorage.getItem('token')}`}},
-        ).then((response) => {
-            setInvoices(response.data)
-        }).catch((error) => {
-            console.log("Something went wrong. May be auth token is invalid.")
-        })
-    }
-    React.useEffect(() => getProducts(), [])
-    React.useEffect(() => getInvoices(), [newInvoice])
+
+    React.useEffect(() => {getProducts()}, [])
     function handleChange(event) {
         const {name, value} = event.target
         setInvoiceProduct(prev => ({...prev, [name]: value}))
     }
     function handleSubmit(event) {
-        event.preventDefault()
+        event.preventDefault();
         setInvoiceProduct(prev => ({
-        ...prev, products_left: prev.quantity, price: prev.price * 100
+            ...prev,
+            products_left: prev.quantity,
+            price: prev.price * 100,
+            purchase_invoice_id: props.invoiceId
         }))
-        sendPurchaseInvoiceProduct()
-        setInvoiceProduct({
-            product_id: '',
-            quantity: '',
-            price: '',
-            purchase_invoice_id: '',
-            products_left: '',
-        })
     };
+    React.useEffect(() => {
+        if (Boolean(invoiceProduct.purchase_invoice_id)) sendPurchaseInvoiceProduct()
+    }, [invoiceProduct.purchase_invoice_id])
     return (
-        <form onSubmit={handleSubmit}>
-            <select
-                name="product_id"
-                id="idProduct"
-                onChange={handleChange}
-                value={invoiceProduct.product_id}
-            >
-                <option>Select product</option>
-                {products.map((product, i) => {
-                    return (
-                        <option
-                            value={product.id}
-                            key={i}
+        <form
+            onSubmit={handleSubmit}
+            className="modal fade"
+            id="CreatePurchaseInvoiceProductModal"
+            tabIndex="-1"
+            aria-labelledby="CreatePurchaseInvoiceProductModalLabel"
+            aria-hidden="true"
+        >
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h1
+                            className="modal-title fs-5"
+                            id="CreatePurchaseInvoiceProductModalLabel"
+                        >Додати товар</h1>
+                        <button
+                            type="button"
+                            className="btn-close"
+                            data-bs-dissmiss="modal"
+                            aria-label="Close"
+                        ></button>
+                    </div>
+                    <div className="modal-body">
+                        <select
+                            name="product_id"
+                            id="idProduct"
+                            onChange={handleChange}
+                            value={invoiceProduct.product_id}
                         >
-                            {`${product.code} ${product.name}`}
-                        </option>
-                    )
-                })}
-            </select>
-            <input
-                type="number"
-                name="quantity"
-                required
-                id="idQuantity"
-                placeholder="Quantity"
-                onChange={handleChange}
-                value={invoiceProduct.quantity}
-            />
-            <input
-                type="number"
-                name="price"
-                required
-                step="0.01"
-                id="idPrice"
-                placeholder="Price"
-                onChange={handleChange}
-                value={invoiceProduct.price}
-            />
-            <select
-                name="purchase_invoice_id"
-                id="idPurchaseInvoice"
-                onChange={handleChange}
-                value={invoiceProduct.purchase_invoice_id}
-            >
-                <option>Select purchase invoice</option>
-
-            </select>
+                            <option>Select product</option>
+                            {products.map((product, i) => {
+                                return (
+                                    <option
+                                        value={product.id}
+                                        key={i}
+                                    >
+                                        {`${product.code} ${product.name}`}
+                                    </option>
+                                )
+                            })}
+                        </select>
+                        <input
+                            type="number"
+                            name="quantity"
+                            required
+                            id="idQuantity"
+                            placeholder="Quantity"
+                            onChange={handleChange}
+                            value={invoiceProduct.quantity}
+                        />
+                        <input
+                            type="number"
+                            name="price"
+                            required
+                            step="0.01"
+                            id="idPrice"
+                            placeholder="Price"
+                            onChange={handleChange}
+                            value={invoiceProduct.price}
+                        />
+                    </div>
+                    <div className="modal-footer">
+                        <button
+                            type="button"
+                            className="btn btn-secondary"
+                            data-bs-dismiss="modal"
+                        >Закрити</button>
+                        <button
+                            type="submit"
+                            className="btn btn-primary"
+                        >Зберегти</button>
+                    </div>
+                </div>
+            </div>
         </form>
     )
 };
